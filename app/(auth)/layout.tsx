3f05ff654586d2322f6f2a431e506fff09e8662e
Natiene <cardosonatiene@gmail.com>
2024-03-23 17:03:43 -0300

    create login and sign-up layouts

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { ReactNode } from "react";
+import BackgroundImage from "../../public/bg-netflix-login.jpg";
+import Logo from "../../public/netflix-logo.svg";
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="relative flex h-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Image
+        src={BackgroundImage}
+        alt="background image"
+        className="hidden sm:flex sm:object-cover -z-10 brightness-50"
+        priority
+        fill
+      />
+      <Image
+        src={Logo}
+        alt="Logo"
+        className="absolute left-4 top-4 object-contain md:left-10 md:top-6"
+        width={120}
+        height={120}
+        priority
+      />
+      {children}
+    </div>
+  );
+}
